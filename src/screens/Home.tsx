47cf6650af43ab400e97b88fa063862080cc0ef3
samuel-akinboro <victorakinboro2003@gmail.com>
2023-07-04 22:51:19 +0100

    infinite scroll

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,67 @@
-import { SafeAreaView, StyleSheet, Text, View, FlatList } from 'react-native'
-import React from 'react'
+import { SafeAreaView, StyleSheet, Text, View, FlatList, RefreshControl, ActivityIndicator } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
 import BookCard from '../components/BookCard'
 
+const DATA_PER_PAGE = 10; // Number of items to fetch per page
+const VIEWABILITY_THRESHOLD = 0.8; // Threshold for triggering data fetch
+
 const Home = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const [page, setPage] = useState(1);
+  const flatListRef = useRef(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      setIsLoading(true);
+
+      // Simulate API call
+      const response = await fetch(`https://jsonplaceholder.typicode.com/photos/?_limit=${page * DATA_PER_PAGE}`);
+      const newData = await response.json();
+
+      setData([...data, ...newData]);
+      setPage((prevPage) => prevPage + 1);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setIsLoading(false);
+      setIsRefreshing(false)
+    }
+  };
+
+  function renderFooter() {
+    if (!isLoading) return null;
+
+    return (
+      <View style={{ paddingVertical: 20 }}>
+        <ActivityIndicator size="large" color="gray" />
+      </View>
+    );
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
-        data={Array(10).fill('')}
+        data={data}
         numColumns={2}
+        ref={flatListRef}
         columnWrapperStyle={{gap: 1}}
+        keyExtractor={(item, i) => `${item?.id}-${i}`}
         renderItem={({item}) => <BookCard />}
+        onEndReached={fetchData}
+        onEndReachedThreshold={0.8} // Define how close to the end before
+        refreshControl={(
+          <RefreshControl
+            refreshing={isRefreshing}
+            onRefresh={fetchData} // Trigger fetchData when the user pulls down to refresh
+          />
+        )}
+        ListFooterComponent={renderFooter}
       />
     </SafeAreaView>
   )
