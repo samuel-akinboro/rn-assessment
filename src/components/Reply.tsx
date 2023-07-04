db62e7e71f1b7aae9dcd63942cc5c6d234df2c03
samuel-akinboro <victorakinboro2003@gmail.com>
2023-07-04 22:02:13 +0100

    few changes

--- src/components/Reply.tsx
+++ src/components/Reply.tsx
@@ -6,7 +6,7 @@ const Reply = () => {
   return (
     <View style={styles.container}>
       <Image
-        source={require('../../assets/images/avatar1.png')}
+        source={require('../../assets/images/avatar2.png')}
         style={styles.avatar}
       />
       <View style={styles.details}>
@@ -21,10 +21,7 @@ const Reply = () => {
           </TouchableOpacity>
         </View>
         <Text style={styles.description}>
-          어머 제가 있던 테이블이 제일 반응이 좋았나보네요🤭 
-          우짤래미님도 아시겠지만 저도 일반인 몸매 그 이상도 이하도
-          아니잖아요?! 그런 제가 기꺼이 도전해봤는데 생각보다
-          괜찮았어요! 오늘 중으로 라이브 리뷰 올라온다고 하니
+          어머 제가 있던 테이블이 제일 반응이 좋았나보네요🤭
           꼭 봐주세용~!
         </Text>
         <View style={styles.footer}>
